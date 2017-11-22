86c869a1c3b8c617d3e6c58719178bc12e64f596
toxicologist <toxiccologist@gmail.com>
2017-11-22 19:29:40 -0200

    Added ammoreloaded.io as seednode

--- src/net.cpp
+++ src/net.cpp
@@ -1210,7 +1210,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    	{"52.43.70.234", "52.43.70.234"},
+        {"ammoreloaded.io", "ammoreloaded.io"},
 		{"", ""},
 };
 
